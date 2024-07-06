c2d7d10337497be851d519eee22611ee8ba4d487
Spyro7883 <spinu11_2003@yahoo.ro>
2024-07-06 15:24:02 +0300

    ratelimit implemented

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,6 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
+import { ratelimit } from "~/server/ratelimit";
 
 const f = createUploadthing();
 
@@ -12,6 +13,9 @@ export const ourFileRouter = {
       const user = auth();
 
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const { success } = await ratelimit.limit(user.userId);
+      if (!success) throw new UploadThingError("Ratelimited");
 
       return { userId: user.userId };
     })
